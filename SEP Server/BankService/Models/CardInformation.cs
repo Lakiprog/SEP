8e693e7c1b9a5bc378a25e37d0232d663a41c3a8
elcholo01 <dvdcolak@gmail.com>
2024-11-12 21:33:32 +0100

    Start Bank

---
+++ SEP Server/BankService/Models/CardInformation.cs
@@ -0,0 +1,12 @@
+﻿using System.Security.Principal;
+
+namespace BankService.Models
+{
+    public class CardInformation
+    {
+        public string CardNumber { get; set; }
+        public string CardHolderName { get; set; }
+        public string ExpiryDate { get; set; }
+        public string CVC { get; set; }
+    }
+}
